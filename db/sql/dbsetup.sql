904e00aa04e6e7c09ec901a8b8f2a44267e87650
shrilekkala <shri.lekkala@gmail.com>
2026-05-14 16:38:46 -0500

    Fix mv_validation_results_plot definition
    - brings it in line with most recent migration

--- db/sql/dbsetup.sql
+++ db/sql/dbsetup.sql
@@ -1561,6 +1561,8 @@ WITH DATA;
  CREATE INDEX idx_mv_endpoint_list_org_vendor ON mv_endpoint_list_organizations(vendor_name);
  CREATE INDEX idx_mv_endpoint_list_org_url ON mv_endpoint_list_organizations(url);
 
+DROP MATERIALIZED VIEW IF EXISTS mv_validation_results_plot CASCADE;
+
 CREATE MATERIALIZED VIEW mv_validation_results_plot AS
 SELECT DISTINCT t.url,
 t.fhir_version,
@@ -1571,12 +1573,13 @@ t.expected,
 t.actual,
 t.comment,
 t.reference
-FROM ( SELECT COALESCE(vendors.name, 'Unknown'::character varying) AS vendor_name,
+FROM (SELECT DISTINCT ON (f.url, f.requested_fhir_version, v.validation_result_id, v.rule_name, f.vendor_id)
+        COALESCE(vendors.name, 'Unknown'::character varying) AS vendor_name,
         f.url,
             CASE
                 WHEN f.capability_fhir_version::text = ''::text THEN 'No Cap Stat'::character varying
                 WHEN "position"(f.capability_fhir_version::text, '-'::text) > 0 THEN "substring"(f.capability_fhir_version::text, 1, "position"(f.capability_fhir_version::text, '-'::text) - 1)::character varying
-                WHEN f.capability_fhir_version::text <> ALL (ARRAY['0.4.0'::character varying, '0.5.0'::character varying, '1.0.0'::character varying, '1.0.1'::character varying, '1.0.2'::character varying, '1.1.0'::character varying, '1.2.0'::character varying, '1.4.0'::character varying, '1.6.0'::character varying, '1.8.0'::character varying, '3.0.0'::character varying, '3.0.1'::character varying, '3.0.2'::character varying, '3.2.0'::character varying, '3.3.0'::character varying, '3.5.0'::character varying, '3.5a.0'::character varying, '4.0.0'::character varying, '4.0.1'::character varying, '4.1.0'::character varying, '4.3.0'::character varying, '4.2.0'::character varying, '4.4.0'::character varying, '4.5.0'::character varying, '4.6.0'::character varying, '5.0.0'::character varying]::text[]) THEN 'Unknown'::character varying
+                WHEN f.capability_fhir_version::text <> ALL (ARRAY['0.4.0'::character varying, '0.4'::character varying, '0.5.0'::character varying, '0.5'::character varying, '1.0.0'::character varying, '1.0'::character varying, '1'::character varying, '1.0.1'::character varying, '1.0.2'::character varying, '1.1.0'::character varying, '1.1'::character varying, '1.2.0'::character varying, '1.2'::character varying, '1.4.0'::character varying, '1.4'::character varying, '1.6.0'::character varying, '1.6'::character varying, '1.8.0'::character varying, '1.8'::character varying, '3.0.0'::character varying, '3.0'::character varying, '3'::character varying, '3.0.1'::character varying, '3.0.2'::character varying, '3.2.0'::character varying, '3.2'::character varying, '3.3.0'::character varying, '3.3'::character varying, '3.5.0'::character varying, '3.5'::character varying, '3.5a.0'::character varying, '4.0.0'::character varying, '4.0'::character varying, '4'::character varying, '4.0.1'::character varying, '4.1.0'::character varying, '4.1'::character varying, '4.3.0'::character varying, '4.3'::character varying, '4.2.0'::character varying, '4.2'::character varying, '4.4.0'::character varying, '4.4'::character varying, '4.5.0'::character varying, '4.5'::character varying, '4.6.0'::character varying, '4.6'::character varying, '5.0.0'::character varying, '5.0'::character varying, '5'::character varying]::text[]) THEN 'Unknown'::character varying
                 ELSE f.capability_fhir_version
             END AS fhir_version,
         v.rule_name,
@@ -1590,7 +1593,7 @@ FROM ( SELECT COALESCE(vendors.name, 'Unknown'::character varying) AS vendor_nam
         FROM fhir_endpoints_info f
             JOIN validations v ON f.validation_result_id = v.validation_result_id
             LEFT JOIN vendors ON f.vendor_id = vendors.id
-        ORDER BY v.validation_result_id, v.rule_name) t;
+        ORDER BY f.url, f.requested_fhir_version, v.validation_result_id, v.rule_name, f.vendor_id) t;
 
 CREATE UNIQUE INDEX mv_validation_results_plot_unique_idx 
 ON mv_validation_results_plot(url, fhir_version, vendor_name, rule_name, valid, expected, actual);
